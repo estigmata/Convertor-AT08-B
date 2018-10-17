34ad6de9db85d44feaf2dddd9bc207480943e24b
estigmata <nestor.otondo@fundacion-jala.org>
2018-10-17 01:28:04 -0400

    Implement singleton

--- src/main/java/org/fundacionjala/convertor/Main.java
+++ src/main/java/org/fundacionjala/convertor/Main.java
@@ -1,7 +1,21 @@
 package org.fundacionjala.convertor;
 
+import org.fundacionjala.convertor.utils.AbstractLogger;
+
+import java.util.logging.SimpleFormatter;
+
+import static java.util.logging.Level.FINER;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.print("Hello camaradas!");
+    AbstractLogger myLogger = AbstractLogger.getInstance();
+    myLogger.setFormatter(new SimpleFormatter());
+    myLogger.setPath("C:/Temp");
+    myLogger.setFilename("converter.log");
+    myLogger.setClass(Main.class.getName());
+    myLogger.setLogger();
+    myLogger.setLevel(FINER);
+    System.out.print("Hello comrades!");
+    myLogger.logMessage("The app show 'Hello comrades!'");
   }
 }
