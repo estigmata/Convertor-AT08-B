4471130d9f5ee215dd64b567aba76310736be06e
Rodrigo Menacho <rmenacho21@gmail.com>
2018-10-18 02:36:22 -0400

    Fix minor

--- src/main/java/org/fundacionjala/convertor/Main.java
+++ src/main/java/org/fundacionjala/convertor/Main.java
@@ -4,6 +4,6 @@ import org.fundacionjala.convertor.view.Viewer;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello camaradas!");
+      System.out.print("Hello camaradas!");
     }
 }
