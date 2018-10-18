ce3186094b40201c9a398d85dbbb414dfca62429
geremygustavo <abelmallcu123@gmail.com>
2018-10-18 15:39:06 -0400

    commit model 10/18/2018 - 3

--- src/main/java/org/fundacionjala/convertor/model/MediaFileModel.java
+++ src/main/java/org/fundacionjala/convertor/model/MediaFileModel.java
@@ -5,13 +5,20 @@ import java.util.ArrayList;
 
 
 /**
- * this instance instance MediaFile instances
+ * MediaFileModel.java Copyright (c) 2018 Jalasoft.
+ * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ * This software is the confidential and proprietary information of
+ * Jalasoft, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
  */
 public class MediaFileModel {
 
     /**
-     * @param path input string showing the route
-     * @return value return
+     * @param path input string showing the route.
+     * @return value return.
      */
     public ArrayList<File> searchFiles(final String path) {
         ArrayList<File> fileArrayList = new ArrayList<>();
@@ -22,8 +29,6 @@ public class MediaFileModel {
                 fileArrayList.addAll(searchFiles(index.getPath()));
             }
             fileArrayList.add(index);
-            System.out.println("Path :" + index.getPath() + " --> Name " + index.getName());
-
         }
         return fileArrayList;
     }
