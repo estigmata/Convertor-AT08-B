2db6fcdcb668530bdf2a251698b6d6f2df281f20
geremygustavo <abelmallcu123@gmail.com>
2018-10-18 01:18:38 -0400

    commit 4 model

---
+++ src/main/java/org/fundacionjala/convertor/model/MediaFileModel.java
@@ -0,0 +1,22 @@
+package org.fundacionjala.convertor.model;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * this instance instance MediaFile instances, its attributes are name and path
+ */
+public class MediaFileModel {
+
+    private String name;
+    private String path;
+
+    /**
+     * @param parameter path of input
+     * @return return value
+     */
+    public List<MediaFileModel> searchFile(final String parameter) {
+        List<MediaFileModel> list = null;
+        return list;
+    }
+}
