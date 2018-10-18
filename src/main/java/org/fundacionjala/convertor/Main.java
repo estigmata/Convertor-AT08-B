256d07e9628e7d752eceb63486fef5f62bbcf89d
Rodrigo Menacho <42896616+Warchino@users.noreply.github.com>
2018-10-18 02:41:22 -0400

    Update Main.java

--- src/main/java/org/fundacionjala/convertor/Main.java
+++ src/main/java/org/fundacionjala/convertor/Main.java
@@ -1,7 +1,5 @@
 package org.fundacionjala.convertor;
 
-import org.fundacionjala.convertor.view.Viewer;
-
 public class Main {
   public static void main(String[] args) {
     System.out.print("Hello camaradas!");
