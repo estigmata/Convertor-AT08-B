66a47f3fa459ef5540b7217722861732626d3e08
Rodrigo Menacho <rmenacho21@gmail.com>
2018-10-19 10:37:00 -0400

    Resolve conflicts

--- src/main/java/org/fundacionjala/convertor/utils/AbstractLogger.java
+++ src/main/java/org/fundacionjala/convertor/utils/AbstractLogger.java
@@ -1,16 +1,18 @@
 /**
- * @(#)Main.java Copyright (c) 2018 Fundacion Jala. All rights reserved.
+ *
+ * @(#)AbstractLogger.java Copyright (c) 2018 Fundacion Jala. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- * <p>
+ *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * version 2 for more details (a copy is included in the LICENSE file that
  * accompanied this code).
- * <p>
+ *
  * Please contact Fundacion Jala, 2643 Av Melchor Perez de Olguin, Colquiri
  * Sud, Cochabamba, Bolivia. www.fundacion-jala.org if you need additional
  * information or have any questions.
+ *
  */
 
 package org.fundacionjala.convertor.utils;
