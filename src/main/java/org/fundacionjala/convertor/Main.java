db60ca0cd6a7b70f9228ba40ef8a5a3816c7674a
Rodrigo Menacho <rmenacho21@gmail.com>
2018-10-25 11:41:21 -0400

    Restablished the main

--- src/main/java/org/fundacionjala/convertor/Main.java
+++ src/main/java/org/fundacionjala/convertor/Main.java
@@ -1,16 +1,18 @@
 /**
+ *
  * @(#)Main.java Copyright (c) 2018 Fundacion Jala. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- * <p>
+ *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * version 2 for more details (a copy is included in the LICENSE file that
  * accompanied this code).
- * <p>
+ *
  * Please contact Fundacion Jala, 2643 Av Melchor Perez de Olguin, Colquiri
  * Sud, Cochabamba, Bolivia. www.fundacion-jala.org if you need additional
  * information or have any questions.
+ *
  */
 
 package org.fundacionjala.convertor;
@@ -19,31 +21,31 @@ import org.fundacionjala.convertor.controller.ConvertorController;
 import org.fundacionjala.convertor.utils.AbstractLogger;
 
 /**
+ *
  * Class Main.
  *
  * @author Nestor Otondo [nestor.otondo@fundacion-jala.org]
  * @version 1.0
+ *
  */
 public final class Main {
-    private static AbstractLogger log;
+  private static AbstractLogger log;
 
-    /**
-     * Constructor not called.
-     */
-    private Main() {
-    }
+  /**
+   * Constructor not called.
+   */
+  private Main() { }
 
-    /**
-     * Main method.
-     *
-     * @param args type array of String.
-     */
-    public static void main(final String[] args) {
-        log = AbstractLogger.getInstance();
-        ConvertorController controller = new ConvertorController();
-        controller.actionConverter();
-        log.setLogger(ConvertorController.class.getName());
-        log.info();
-    }
+  /**
+   * Main method.
+   * @param args type array of String.
+   */
+  public static void main(final String[] args) {
+    log = AbstractLogger.getInstance();
+    ConvertorController controller = new ConvertorController();
+    controller.actionConverter();
+    log.setLogger(ConvertorController.class.getName());
+    log.info();
+  }
 
 }
