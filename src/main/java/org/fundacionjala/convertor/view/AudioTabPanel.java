7dce5939dae760871ff7d2534ef99f49bae9a362
estigmata <nestor.otondo@fundacion-jala.org>
2018-10-25 02:02:35 -0400

    Embeded media player panel WIP

--- src/main/java/org/fundacionjala/convertor/view/AudioTabPanel.java
+++ src/main/java/org/fundacionjala/convertor/view/AudioTabPanel.java
@@ -17,6 +17,8 @@ package org.fundacionjala.convertor.view;
 import org.fundacionjala.convertor.utils.AbstractLogger;
 import org.fundacionjala.convertor.view.finder.AudioFinderPanel;
 import org.fundacionjala.convertor.view.finder.FinderPanel;
+import uk.co.caprica.vlcj.component.EmbeddedMediaListPlayerComponent;
+import uk.co.caprica.vlcj.discovery.NativeDiscovery;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
@@ -27,6 +29,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.nio.file.Paths;
 
 
 /**
@@ -36,102 +39,109 @@ import java.awt.GridBagLayout;
  * @version 1.0
  */
 class AudioTabPanel extends JPanel {
-    private FinderPanel finderPanel;
-    private ResultTable resultTableAudio;
-    private JScrollPane resultScrollTable;
+  private FinderPanel finderPanel;
+  private ResultTable resultTableAudio;
+  private JScrollPane resultScrollTable;
+  private EmbeddedMediaListPlayerComponent playerComponent;
 
-    /**
-     * Constructor.
-     */
-    AudioTabPanel() {
-        AbstractLogger log = AbstractLogger.getInstance();
-        log.setLogger(AudioTabPanel.class.getName());
-        this.setLayout(new GridBagLayout());
-        finderPanel = new AudioFinderPanel();
-        finderPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        resultTableAudio = new ResultTable();
-        resultScrollTable = new JScrollPane(resultTableAudio);
-        final int tWidth = 200;
-        final int tHeight = 50;
-        resultScrollTable.setPreferredSize(new Dimension(tWidth, tHeight));
-        initComponents();
-        log.info();
-    }
+  /**
+   * Constructor.
+   */
+  AudioTabPanel() {
+    new NativeDiscovery().discover();
+    AbstractLogger log = AbstractLogger.getInstance();
+    log.setLogger(AudioTabPanel.class.getName());
+    this.setLayout(new GridBagLayout());
+    finderPanel = new AudioFinderPanel();
+    finderPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    resultTableAudio = new ResultTable();
+    resultScrollTable = new JScrollPane(resultTableAudio);
+    final int tWidth = 200;
+    final int tHeight = 50;
+    resultScrollTable.setPreferredSize(new Dimension(tWidth, tHeight));
+    initComponents();
+    log.info();
+  }
 
-    /**
-     * Initialization of Components.
-     */
-    private void initComponents() {
-        final int ten = 10;
-        final int three = 3;
-        final int thirteen = 30;
-        final int threeHundred = 300;
-        finderPanel.initBasicComponents();
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.ipady = ten;
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        this.add(new JLabel("Search Panel"), constraints);
-        constraints.gridx = 1;
-        constraints.gridy = 0;
-        this.add(new JLabel("Reproductor Panel"), constraints);
-        constraints.ipady = 0;
-        constraints.gridx = 0;
-        constraints.gridy = 1;
-        this.add(finderPanel, constraints);
-        constraints.gridx = 1;
-        constraints.gridy = 1;
-        constraints.ipadx = threeHundred;
-        JPanel tempReproductor = new JPanel();
-        tempReproductor.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        this.add(tempReproductor, constraints); //Reproductor Panel
-        constraints.ipadx = 0;
-        constraints.gridx = 0;
-        constraints.gridy = 2;
-        this.add(new JLabel("Showed Results:"), constraints);
-        constraints.gridx = 1;
-        constraints.gridy = 2;
-        this.add(new JLabel("Information"), constraints);
-        constraints.ipady = threeHundred;
-        constraints.weighty = thirteen;
-        constraints.gridx = 0;
-        constraints.gridy = three;
-        this.add(resultScrollTable, constraints);
-        constraints.gridx = 1;
-        constraints.gridy = three;
-        JPanel tempInformation = new JPanel();
-        tempInformation.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        this.add(tempInformation, constraints); // Information Panel
+  /**
+   * Initialization of Components.
+   */
+  private void initComponents() {
+    final int ten = 10;
+    final int three = 3;
+    final int thirteen = 30;
+    final int threeHundred = 300;
+    finderPanel.initBasicComponents();
+    GridBagConstraints constraints = new GridBagConstraints();
+    constraints.fill = GridBagConstraints.HORIZONTAL;
+    constraints.ipady = ten;
+    constraints.gridx = 0;
+    constraints.gridy = 0;
+    this.add(new JLabel("Search Panel"), constraints);
+    constraints.gridx = 1;
+    constraints.gridy = 0;
+    this.add(new JLabel("Reproductor Panel"), constraints);
+    constraints.ipady = 0;
+    constraints.gridx = 0;
+    constraints.gridy = 1;
+    this.add(finderPanel, constraints);
+    constraints.gridx = 1;
+    constraints.gridy = 1;
+    constraints.ipadx = threeHundred;
+    JPanel playerPanel = new JPanel();
+    playerComponent = new EmbeddedMediaListPlayerComponent();
+    playerPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    //playerPanel.add(new JLabel("Here my code"), constraints);
+    playerPanel.add(playerComponent);
+    playerPanel.setVisible(true);
+    playerComponent.getMediaPlayer().playMedia((Paths.get("C:\\Videos\\Test.mp4").toUri().toString()));
+    this.add(playerPanel, constraints);
+    constraints.ipadx = 0;
+    constraints.gridx = 0;
+    constraints.gridy = 2;
+    this.add(new JLabel("Showed Results:"), constraints);
+    constraints.gridx = 1;
+    constraints.gridy = 2;
+    this.add(new JLabel("Information"), constraints);
+    constraints.ipady = threeHundred;
+    constraints.weighty = thirteen;
+    constraints.gridx = 0;
+    constraints.gridy = three;
+    this.add(resultScrollTable, constraints);
+    constraints.gridx = 1;
+    constraints.gridy = three;
+    JPanel tempInformation = new JPanel();
+    tempInformation.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    this.add(tempInformation, constraints); // Information Panel
 
 
-    }
+  }
 
-    /**
-     * Getter of default table model.
-     *
-     * @return the default table.
-     */
-    public DefaultTableModel getDefaultTableModel() {
-        return resultTableAudio.getDefaultTableModel();
-    }
+  /**
+   * Getter of default table model.
+   *
+   * @return the default table.
+   */
+  public DefaultTableModel getDefaultTableModel() {
+    return resultTableAudio.getDefaultTableModel();
+  }
 
-    /**
-     * Getter FinderClass.
-     *
-     * @return FinderPanel.
-     */
-    public FinderPanel getFinderPanel() {
-        return finderPanel;
-    }
+  /**
+   * Getter FinderClass.
+   *
+   * @return FinderPanel.
+   */
+  public FinderPanel getFinderPanel() {
+    return finderPanel;
+  }
 
-    /**
-     * Setter the default table mode.
-     *
-     * @param defaultTableModel input object.
-     */
-    public void setDefaultTableModel(final DefaultTableModel defaultTableModel) {
-        resultTableAudio.setDefaultTableModel(defaultTableModel);
-    }
+  /**
+   * Setter the default table mode.
+   *
+   * @param defaultTableModel input object.
+   */
+  public void setDefaultTableModel(final DefaultTableModel defaultTableModel) {
+    resultTableAudio.setDefaultTableModel(defaultTableModel);
+  }
 
 }
